f5fde5a90763099c52ac65578034e7e5fc0ea825
Stephen Rogers <srgers10@memphis.edu>
2020-03-15 22:27:53 -0500

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+var video;
+var socket;
+var displayMediaOptions = {
+  video: {
+    cursor: "always"
+  },
+  audio: false
+};
+var btn_startCapture;
+var chk_isTeacher;
+
+function setup(){
+  canvas = createCanvas(320, 240);
+  background(51);
+
+
+  socket = io.connect('75.64.127.144:3000')
+  socket.on('stream-out', renderStream)
+  console.log("Connected!");
+
+
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+  img = loadImage("buffer.jpg")
+
+  btn_startCapture = createButton("Share Screen");
+  btn_startCapture.mousePressed(startCapture)
+  chk_isTeacher = createCheckbox("Stream", false)
+}
+function draw() {
+  image(video, 0,0)
+  if(chk_isTeacher.checked())
+    socket.emit('stream-in', canvas.elt.toDataURL('image/webp'));
+}
+
+function renderStream(data){
+  document.getElementById("stream_img").src = data
+}
+
+async function startCapture() {
+  try {
+    video.elt.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+    //dumpOptionsInfo();
+    console.log("Starting Capture")
+  } catch(err) {
+    console.error("Error: " + err);
+  }
+}
+
+function stopCapture(evt) {
+  let tracks = video.srcObject.getTracks();
+
+  tracks.forEach(track => track.stop());
+  video.srcObject = null;
+}
---
+++ server.js
@@ -0,0 +1,24 @@
+
+var express = require('express');
+
+var app = express();
+var server = app.listen(3000);
+
+app.use(express.static('public'));
+
+
+console.log("My server is running!")
+
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+  console.log("new connection: " + socket.id);
+  socket.on('stream-in', streamMsg);
+  function streamMsg(data){
+    socket.broadcast.emit('stream-out', data);
+    //console.log(data)
+  }
+}
